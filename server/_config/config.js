6d5c711deed323985e3191e78d780ad2d9758383
EC2 Default User <rkylerd06@gmail.com>
2020-02-19 15:14:21 +0000

    changed from pat to kyler

--- server/_config/config.js
+++ server/_config/config.js
@@ -3,22 +3,22 @@ const testDbName = 'todo-test'
 
 export const configOptions = {
   development: {
-    db: `mongodb://localhost/${dbName}`,
+    db: `mongodb://rkylerd06:vbyBhOKEXS9OCSPG@cluster0-shard-00-00-8lbzh.mongodb.net:27017,cluster0-shard-00-01-8lbzh.mongodb.net:27017,cluster0-shard-00-02-8lbzh.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   testDevelopment: {
-    db: `mongodb://localhost/${testDbName}`,
+    db: `mongodb://rkylerd06:vbyBhOKEXS9OCSPG@cluster0-shard-00-00-8lbzh.mongodb.net:27017,cluster0-shard-00-01-8lbzh.mongodb.net:27017,cluster0-shard-00-02-8lbzh.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   production: {
-    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb://rkylerd06:vbyBhOKEXS9OCSPG@cluster0-shard-00-00-8lbzh.mongodb.net:27017,cluster0-shard-00-01-8lbzh.mongodb.net:27017,cluster0-shard-00-02-8lbzh.mongodb.net:27017/${dbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   },
   testProduction: {
-    db: `mongodb://pdrean4:willchangesoon@cluster0-shard-00-00-pg36l.mongodb.net:27017,cluster0-shard-00-01-pg36l.mongodb.net:27017,cluster0-shard-00-02-pg36l.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb://rkylerd06:vbyBhOKEXS9OCSPG@cluster0-shard-00-00-8lbzh.mongodb.net:27017,cluster0-shard-00-01-8lbzh.mongodb.net:27017,cluster0-shard-00-02-8lbzh.mongodb.net:27017/${testDbName}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     port: process.env.PORT || 3001,
     userCookieName: 'user'
   }
